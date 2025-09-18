35c489391156aada89c32b75d9f1735736108fbb
chithuandev <tranchithuanjr@gmail.com>
2025-09-18 12:26:14 +0700

    remove OnConfiguring DI in program.cs

--- Serein.Candle.Infrastructure/Persistence/Models/CandleShopDbContext.cs
+++ Serein.Candle.Infrastructure/Persistence/Models/CandleShopDbContext.cs
@@ -63,10 +63,6 @@ public partial class CandleShopDbContext : DbContext
 
     public virtual DbSet<Wishlist> Wishlists { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-BRF7Q691;Database=CandleShopDB;Trusted_Connection=True;Encrypt=False;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AuditLog>(entity =>
