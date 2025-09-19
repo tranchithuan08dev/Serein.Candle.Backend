2a50e16aaede84be0db96cf4687e8b856218f2eb
chithuandev <tranchithuanjr@gmail.com>
2025-09-19 12:27:07 +0700

    create loginReposeDto

---
+++ Serein.Candle.Domain/DTOs/LoginResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Domain.DTOs
+{
+    public class LoginResponseDto
+    {
+    
+        public string Token { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string RoleName { get; set; } = string.Empty;
+    }
+}
