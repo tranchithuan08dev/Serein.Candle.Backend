62bdc1066fd1e769461b84a6cf660f384bd2005a
chithuandev <tranchithuanjr@gmail.com>
2025-09-19 17:08:46 +0700

    create interface and service of email

---
+++ Serein.Candle.Infrastructure/Services/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using Microsoft.Extensions.Options;
+using Serein.Candle.Domain.Settings;
+using Serein.Candle.Infrastructure.Interfaces;
+
+namespace Serein.Candle.Infrastructure.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly SmtpSettings _smtpSettings;
+
+        public EmailService(IOptions<SmtpSettings> smtpSettings)
+        {
+            _smtpSettings = smtpSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_smtpSettings.SenderEmail));
+            email.To.Add(MailboxAddress.Parse(toEmail));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder
+            {
+                HtmlBody = body,
+                TextBody = "This is the plain text version of the email."
+            };
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(_smtpSettings.Server, _smtpSettings.Port, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_smtpSettings.Username, _smtpSettings.Password);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+    }
+}
