f035eea7765b52494f43d4dac718704b5c88c1a0
chithuandev <tranchithuanjr@gmail.com>
2025-09-19 17:08:22 +0700

    create config smtpseting

---
+++ Serein.Candle.Domain/Settings/SmtpSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Domain.Settings
+{
+    public class SmtpSettings
+    {
+        public string Server { get; set; } = string.Empty;
+        public int Port { get; set; }
+        public string SenderName { get; set; } = string.Empty;
+        public string SenderEmail { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
