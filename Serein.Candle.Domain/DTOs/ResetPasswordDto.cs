819edd856b84cdbfe3b6c91b371c6881c47fda7c
chithuandev <tranchithuanjr@gmail.com>
2025-09-19 17:07:59 +0700

    create resetpassword

---
+++ Serein.Candle.Domain/DTOs/ResetPasswordDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Domain.DTOs
+{
+    public class ResetPasswordDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Otp { get; set; } = string.Empty;
+
+        [Required]
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
