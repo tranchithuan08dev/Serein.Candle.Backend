17dc8eb73f55147cb3ec7fd29d55f48e5c0fca84
chithuandev <tranchithuanjr@gmail.com>
2025-09-19 14:41:15 +0700

    create DTO registerStaff

---
+++ Serein.Candle.Domain/DTOs/RegisterStaffDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Domain.DTOs
+{
+    public class RegisterStaffDto
+    {
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+        [Phone]
+        [Required]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string EmployeeCode { get; set; } = string.Empty;
+
+        [Required]
+        public string Position { get; set; } = string.Empty;
+    }
+}
