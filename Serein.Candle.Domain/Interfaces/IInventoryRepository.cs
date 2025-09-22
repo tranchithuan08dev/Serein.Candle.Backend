807292a400d5f2029632ec695a45baaf5abec299
chithuandev <tranchithuanjr@gmail.com>
2025-09-22 22:25:24 +0700

    create interface

---
+++ Serein.Candle.Domain/Interfaces/IInventoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Serein.Candle.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Domain.Interfaces
+{
+    public interface IInventoryRepository
+    {
+        Task AddInventoryAsync(Inventory inventory);
+       
+    }
+}
---
+++ Serein.Candle.Domain/Interfaces/IProductAttributeValueRepository.cs
@@ -0,0 +1,14 @@
+﻿using Serein.Candle.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Domain.Interfaces
+{
+    public interface IProductAttributeValueRepository
+    {
+        Task AddProductAttributeValueAsync(ProductAttributeValue productAttributeValue);
+    }
+}
