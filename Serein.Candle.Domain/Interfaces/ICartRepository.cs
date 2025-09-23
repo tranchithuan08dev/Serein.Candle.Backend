83c2e818c4dbb980e123bbf99ee7e4b877d7ce50
chithuandev <tranchithuanjr@gmail.com>
2025-09-23 21:25:44 +0700

    create Icart and ICartItem to create cart

---
+++ Serein.Candle.Domain/Interfaces/ICartRepository.cs
@@ -0,0 +1,16 @@
+﻿using Serein.Candle.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Domain.Interfaces
+{
+    public interface ICartRepository
+    {
+        Task<Cart?> GetCartByUserIdAsync(int userId);
+        Task AddCartAsync(Cart cart);
+        Task<bool> SaveChangesAsync();
+    }
+}
