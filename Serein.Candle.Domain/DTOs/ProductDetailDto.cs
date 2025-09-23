1a3f54d5dcdf74d74b0be39d69f7d9537165141e
chithuandev <tranchithuanjr@gmail.com>
2025-09-23 16:27:09 +0700

    create product detail dto

---
+++ Serein.Candle.Domain/DTOs/ProductDetailDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Domain.DTOs
+{
+    public class ProductDetailDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string SKU { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Ingredients { get; set; } = string.Empty;
+        public string BurnTime { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public ICollection<ProductAttributeDto> Attributes { get; set; } = new List<ProductAttributeDto>();
+        public ICollection<ProductImageDto> Images { get; set; } = new List<ProductImageDto>();
+    }
+
+    public class ProductAttributeDto
+    {
+        public string AttributeName { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+
+    public class ProductImageDto
+    {
+        public string ImageUrl { get; set; } = string.Empty;
+        public bool IsPrimary { get; set; }
+    }
+}
