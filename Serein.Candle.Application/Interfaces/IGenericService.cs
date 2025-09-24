570d0658fc1744dbc7571b928b9dc694c11e2e6a
chithuandev <tranchithuanjr@gmail.com>
2025-09-24 20:12:14 +0700

    create iterface IGenericService

---
+++ Serein.Candle.Application/Interfaces/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Application.Interfaces
+{
+    public interface IGenericService<T, TDto> where T : class
+    {
+        Task<IEnumerable<TDto>> GetAllAsync();
+        Task<TDto?> GetByIdAsync(int id);
+        Task<bool> AddAsync(TDto dto);
+        Task<bool> UpdateAsync(int id, TDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
