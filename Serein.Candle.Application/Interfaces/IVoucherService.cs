24d7d058ab3cd0239050cf28d976a003b7cb8f00
chithuandev <tranchithuanjr@gmail.com>
2025-09-24 21:16:26 +0700

    implement IGenericService

---
+++ Serein.Candle.Application/Interfaces/IVoucherService.cs
@@ -0,0 +1,15 @@
+﻿using Serein.Candle.Domain.DTOs;
+using Serein.Candle.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Application.Interfaces
+{
+    public interface IVoucherService : IGenericService<Voucher, VoucherCRUDDto>
+    {
+        // Thêm các phương thức đặc thù cho Voucher tại đây nếu cần
+    }
+}
