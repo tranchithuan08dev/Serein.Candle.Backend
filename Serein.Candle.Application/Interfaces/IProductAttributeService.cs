80fe223703bc7cdaa34f178f8520732b346f0953
chithuandev <tranchithuanjr@gmail.com>
2025-09-24 20:12:33 +0700

    create IProductAttributeService implement IGenericService

---
+++ Serein.Candle.Application/Interfaces/IProductAttributeService.cs
@@ -0,0 +1,14 @@
+﻿using Serein.Candle.Domain.DTOs;
+using Serein.Candle.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Application.Interfaces
+{
+    public interface IProductAttributeService : IGenericService<ProductAttribute, ProductAttributeCRUDDto>
+    {
+    }
+}
