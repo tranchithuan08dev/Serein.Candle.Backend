a46bbd6087b79f82d913a1d2ae693c03709cd195
chithuandev <tranchithuanjr@gmail.com>
2025-09-24 20:16:06 +0700

    create GenericRepository

---
+++ Serein.Candle.Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Serein.Candle.Domain.Interfaces;
+using Serein.Candle.Infrastructure.Persistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Infrastructure.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly CandleShopDbContext _context;
+
+        public GenericRepository(CandleShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            // Giả định primary key là 'Id' hoặc có tên chung
+            // Tùy thuộc vào thực thể, bạn có thể cần điều chỉnh
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
