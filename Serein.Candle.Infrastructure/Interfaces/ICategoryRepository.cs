496ec4e5aa364b500ddb4f2e6d0479a807841b0d
chithuandev <tranchithuanjr@gmail.com>
2025-09-24 20:15:37 +0700

    implement IGenericRepository

---
+++ Serein.Candle.Infrastructure/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Serein.Candle.Domain.Entities;
+using Serein.Candle.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Infrastructure.Interfaces
+{
+    public interface ICategoryRepository : IGenericRepository<Category>
+    {
+    }
+}
---
+++ Serein.Candle.Infrastructure/Interfaces/IProductAttributeRepository.cs
@@ -0,0 +1,15 @@
+﻿using Serein.Candle.Domain.Entities;
+using Serein.Candle.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Infrastructure.Interfaces
+{
+    public interface IProductAttributeRepository : IGenericRepository<ProductAttribute>
+    {
+        // Thêm các phương thức đặc thù cho ProductAttribute tại đây nếu cần
+    }
+}
