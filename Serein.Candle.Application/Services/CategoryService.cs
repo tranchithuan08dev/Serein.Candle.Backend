10a8bb70dccb4379a885a95c3b5e0771135c4e70
chithuandev <tranchithuanjr@gmail.com>
2025-09-24 20:13:10 +0700

    create CategoryService implements GenericService

---
+++ Serein.Candle.Application/Services/CategoryService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Serein.Candle.Application.Interfaces;
+using Serein.Candle.Domain.DTOs;
+using Serein.Candle.Domain.Entities;
+using Serein.Candle.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Application.Services
+{
+    public class CategoryService : GenericService<Category, CategoryCRUDDto>, ICategoryService
+    {
+        public CategoryService(IGenericRepository<Category> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+    }
+}
