04ebb21b7397afefb413fa643448775074c71768
chithuandev <tranchithuanjr@gmail.com>
2025-09-24 20:11:55 +0700

    create ICategoryService implement IGenericService

---
+++ Serein.Candle.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using Serein.Candle.Domain.DTOs;
+using Serein.Candle.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Application.Interfaces
+{
+    public interface ICategoryService : IGenericService<Category, CategoryCRUDDto>
+    {
+      
+    }
+}
