171286e180a3836ad87ad2160a7a6261530a33c6
chithuandev <tranchithuanjr@gmail.com>
2025-09-24 20:17:16 +0700

    implement controller GenericRepository

---
+++ Serein.Candle.WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serein.Candle.Application.Interfaces;
+using Serein.Candle.Domain.DTOs;
+using Serein.Candle.Domain.Entities;
+
+namespace Serein.Candle.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : GenericController<Category, CategoryCRUDDto>
+    {
+        public CategoryController(IGenericService<Category, CategoryCRUDDto> service) : base(service)
+        {
+        }
+    }
+}
