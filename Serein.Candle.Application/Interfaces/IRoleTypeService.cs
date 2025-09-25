57e9999b15ce2504da4f9caabda1afe5fb4171bf
chithuandev <tranchithuanjr@gmail.com>
2025-09-25 12:15:49 +0700

    create IRoleTypeService

---
+++ Serein.Candle.Application/Interfaces/IRoleTypeService.cs
@@ -0,0 +1,15 @@
+﻿using Serein.Candle.Domain.DTOs;
+using Serein.Candle.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Application.Interfaces
+{
+    public interface IRoleTypeService : IGenericService<RoleType, RoleTypeCRUDDto>
+    {
+        // Thêm các phương thức đặc thù tại đây nếu cần
+    }
+}
