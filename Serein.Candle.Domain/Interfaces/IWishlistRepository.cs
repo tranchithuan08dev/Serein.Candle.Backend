a76c889a54bdc7863524ca72be88402578b27942
chithuandev <tranchithuanjr@gmail.com>
2025-09-25 13:53:11 +0700

    create interface IWishlistRepository

---
+++ Serein.Candle.Domain/Interfaces/IWishlistRepository.cs
@@ -0,0 +1,21 @@
+﻿using Serein.Candle.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Domain.Interfaces
+{
+    public interface IWishlistRepository
+    { 
+        Task<List<Wishlist>> GetWishlistByUserIdAsync(int userId);
+
+        Task<Wishlist?> GetExistingItemAsync(int userId, int productId);
+
+        Task AddAsync(Wishlist wishlistItem);
+        Task RemoveAsync(Wishlist wishlistItem);
+        Task<bool> DeleteByIdAsync(int wishlistId);
+        Task<bool> SaveChangesAsync();
+    }
+}
