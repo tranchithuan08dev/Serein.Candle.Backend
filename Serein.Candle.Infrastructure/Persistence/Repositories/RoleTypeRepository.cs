c318e8506b7a72dde86773d995f0fa271324b34e
chithuandev <tranchithuanjr@gmail.com>
2025-09-25 12:16:59 +0700

    implements IGenericRepository

---
+++ Serein.Candle.Infrastructure/Persistence/Repositories/RoleTypeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Serein.Candle.Domain.Entities;
+using Serein.Candle.Infrastructure.Interfaces;
+using Serein.Candle.Infrastructure.Persistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Infrastructure.Persistence.Repositories
+{
+    public class RoleTypeRepository : GenericRepository<RoleType>, IRoleTypeRepository
+    {
+        public RoleTypeRepository(CandleShopDbContext context) : base(context)
+        {
+        }
+    }
+}
