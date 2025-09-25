d8be700b0e16f2d25b4cea4fb7be405a724e973f
chithuandev <tranchithuanjr@gmail.com>
2025-09-25 13:51:32 +0700

    create IWishlistService and WishlistService

---
+++ Serein.Candle.Application/Interfaces/IWishlistService.cs
@@ -0,0 +1,16 @@
+﻿using Serein.Candle.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Application.Interfaces
+{
+    public interface IWishlistService
+    {
+        Task<IEnumerable<WishlistDto>> GetUserWishlistAsync(int userId);
+        Task<bool> AddToWishlistAsync(int userId, AddWishlistDto dto);
+        Task<bool> RemoveFromWishlistAsync(int wishlistId);
+    }
+}
