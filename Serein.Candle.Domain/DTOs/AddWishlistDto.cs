8677630281c0ef4fe584fb6fc493d9dc1bca4f94
chithuandev <tranchithuanjr@gmail.com>
2025-09-25 13:52:31 +0700

    create  addWishlistDto

---
+++ Serein.Candle.Domain/DTOs/AddWishlistDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Domain.DTOs
+{
+    public class AddWishlistDto
+    {
+        public int ProductId { get; set; }
+    }
+}
