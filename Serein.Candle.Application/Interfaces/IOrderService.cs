205d403ce9aa101d23ba94b1d967c5f3f6b6ca3f
chithuandev <tranchithuanjr@gmail.com>
2025-10-03 20:55:15 +0700

    Create IOrderService as GetUserOrdersAsync,GetOrderDetailsAsync,CreateOrderAsync

---
+++ Serein.Candle.Application/Interfaces/IOrderService.cs
@@ -0,0 +1,21 @@
+﻿using Serein.Candle.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serein.Candle.Application.Interfaces
+{
+    public interface IOrderService
+    {
+        // Lấy danh sách Order của User
+        Task<IEnumerable<OrderDetailDto>> GetUserOrdersAsync(int userId);
+
+        // Lấy chi tiết Order
+        Task<OrderDetailDto?> GetOrderDetailsAsync(int orderId, int userId);
+
+        // Tạo đơn hàng mới
+        Task<(bool Success, string Message, string OrderCode)> CreateOrderAsync(int userId, OrderCreationDto dto);
+    }
+}
